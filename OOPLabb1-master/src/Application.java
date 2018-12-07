ef21106356893be3d173cca307b8acad19fd2d40
AdamRohdell <rohdell@student.chalmers.se>
2018-12-07 12:48:01 +0100

    Last commit

--- OOPLabb1-master/src/Application.java
+++ OOPLabb1-master/src/Application.java
@@ -5,8 +5,8 @@ public class Application {
     public static void main(String[] args){
         CarController cc = new CarController();
         cc.getCars().add(cc.getFactory().createSaab95());
-        cc.getCars().add(cc.getFactory().createSaab95());
-        cc.getCars().add(cc.getFactory().createSaab95());
+        cc.getCars().add(cc.getFactory().createVolvo240());
+        cc.getCars().add(cc.getFactory().createScania());
         cc.initCarcontroller();
 
     }
